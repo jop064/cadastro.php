4414d61b17d29bae2c87ea9b9e7e5ad606b78ccc
jop064 <joaopedrocnsd2017@gmail.com>
2025-05-19 22:04:30 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";       
+$usuario = "root";         
+$senha = "";               
+$banco = "ecoscaling";    
+
+$conexao = new mysqli($hostname, $usuario, $senha, $banco);
+
+
+
+?>
